041380c97a67643f886212e1c9628626339aa965
Alanoterohs <alanoterohs@gmail.com>
2021-09-09 10:02:01 -0300

    modelo de usuarios

---
+++ server/models/Users.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, Sequelize) => {
+  const UsersModel = sequelize.define('usuarios', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    password: {
+      type: Sequelize.STRING,
+    },
+  });
+  return UsersModel;
+};
