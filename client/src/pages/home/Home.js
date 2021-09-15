100f2f778c44fb99bb6a4dd50bbd30344b049ef5
Alanoterohs <alanoterohs@gmail.com>
2021-09-15 12:11:06 -0300

    fetch balance
    realizo la conexion con fecth para obtener las ultimas 10 operaciones

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import Balance from '../../components/balance/Balance';
 import ContainerLastExpenses  from '../../components/lastExpenses/ContainerLastExpenses';
 import ListExpenses from '../../components/expensesItems/ListExpenses';
@@ -10,14 +12,42 @@ import {
   Typography,
 } from '@material-ui/core';
 
-
-function Home({ operation }) {
+function Home({ operation, setOperation }) {
   const classes = useStyles();
+
+  // export const getIncomes = async (setIncome) => {
+  //   try {
+  //     const response = await fetch('http://localhost:5000/registers/getregisters');
+  //     // console.log(response);
+  //     const data = await response.json();
+  //     setIncome(data);
+  //   } catch (err) {
+  //     console.error(err.message);
+  //   }
+  // };
+
+  const getLastOperations = async () => {
+    try {
+      const res = await fetch('http://localhost:5000/operations/lastOperations', {
+        method: 'GET',
+      });
+      const { getOperations } = await res.json();
+      console.log(getOperations);
+      setOperation(getOperations);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getLastOperations();
+  }, []);
+
   return (
     <Grid className={classes.root}>
     <Balance/>
-    <ContainerLastExpenses>
-      <ListExpenses operation = {operation}/>
+    <ContainerLastExpenses title= 'Últimos 10 registros:'>
+      <ListExpenses operation = {operation} letter = 'balance'/>
     </ContainerLastExpenses>
     </Grid>
   );
